bab31a10127e952107240e9865299c03027c2107
Lehtaan <l3htaan@gmail.com>
2015-09-18 17:47:28 +0200

    Initial Commit

---
+++ IOpenGLTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IOpenGLTutorial
+//
+//  Created by Janis Böhm on 18/09/15.
+//  Copyright (c) 2015 Janis Böhm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
